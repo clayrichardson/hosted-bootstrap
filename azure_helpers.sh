131cee7826c8cdca94551478646d9b2f699528c2
clay richardson <crichardson@pivotal.io>
2016-02-24 11:37:16 -0800

    consolidate env variables
    - remove azure cli login check
    
    [#114245951] create env script should produce a 'source-able' env file which contains all the bootstrap and secret environments.
    
    Signed-off-by: Kai Xiang <kxiang@pivotal.io>

--- azure_helpers.sh
+++ azure_helpers.sh
@@ -1,20 +1,18 @@
 function azure_login() {
-  if ! azure account show; then
-    log_file="${OUTPUT_DIR}/login.log"
-    azure login | tee ${log_file} &
-    sleep 2
-    token=`cat ${log_file} |grep "To sign in"| sed 's/.*code \(.*\) to authenticate.$/\1/'`
+  log_file="${OUTPUT_DIR}/login.log"
+  azure login | tee ${log_file} &
+  sleep 2
+  token=`cat ${log_file} |grep "To sign in"| sed 's/.*code \(.*\) to authenticate.$/\1/'`
 
-    if [ -z "${token}" ]; then
-      echo 'No login token found, exit'
-      exit 1
-    fi
-
-    export AZURE_CLI_TOKEN=${token}
-    $(npm bin)/phantomjs ./phantom.js
-    wait
-    azure config mode arm
+  if [ -z "${token}" ]; then
+    echo 'No login token found, exit'
+    exit 1
   fi
+
+  export AZURE_CLI_TOKEN=${token}
+  $(npm bin)/phantomjs ./phantom.js
+  wait
+  azure config mode arm
 }
 
 function env_check() {
